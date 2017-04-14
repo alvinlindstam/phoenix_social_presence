d68b7b6b3a0145447af297d43212098301476771
Alvin Lindstam <alvin.lindstam@gmail.com>
2017-04-14 10:33:12 +0200

    Add boilerplate channel: mix phx.gen.channel User

---
+++ test/web/channels/user_channel_test.exs
@@ -0,0 +1,28 @@
+defmodule SocialPresence.Web.UserChannelTest do
+  use SocialPresence.Web.ChannelCase
+
+  alias SocialPresence.Web.UserChannel
+
+  setup do
+    {:ok, _, socket} =
+      socket("user_id", %{some: :assign})
+      |> subscribe_and_join(UserChannel, "user:lobby")
+
+    {:ok, socket: socket}
+  end
+
+  test "ping replies with status ok", %{socket: socket} do
+    ref = push socket, "ping", %{"hello" => "there"}
+    assert_reply ref, :ok, %{"hello" => "there"}
+  end
+
+  test "shout broadcasts to user:lobby", %{socket: socket} do
+    push socket, "shout", %{"hello" => "all"}
+    assert_broadcast "shout", %{"hello" => "all"}
+  end
+
+  test "broadcasts are pushed to the client", %{socket: socket} do
+    broadcast_from! socket, "broadcast", %{"some" => "data"}
+    assert_push "broadcast", %{"some" => "data"}
+  end
+end
